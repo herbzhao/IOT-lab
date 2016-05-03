558d89bb5106c30da4080f41c62aeab8a839277f
herbzhao <herbzhao@gmail.com>
2016-05-04 00:27:10 +0100

    Alpha 2.6.2
    Port back to 5000 for debug

--- server.py
+++ server.py
@@ -56,5 +56,5 @@ def contact():
 
 if __name__ == "__main__":
 #	app.run(host='0.0.0.0', port=80, debug=True)
-	app.run(host='10.0.0.1', port=80, debug=True)
+	app.run(host='10.0.0.1', debug=True)
 
